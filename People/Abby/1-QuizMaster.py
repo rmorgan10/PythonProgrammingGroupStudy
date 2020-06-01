4d34c08cc9222f4a5374df79dc087721de93e357
abigailbishop <ms.abigailbishop@gmail.com>
2020-06-01 09:54:50 -0500

    A quiz with the barest of bones

---
+++ People/Abby/1-QuizMaster.py
@@ -0,0 +1,49 @@
+## Quiz testing knowledge of python builtins
+## 2020 June 1
+
+import random
+
+# Identify all Built-ins
+reals = [
+         ["Arithmetic Error", "ans"],
+         ["AssertionError", "ans"],
+         ["AttributeError", "ans"],
+        ]
+
+# Load in all fake Built-Ins
+fakes = [
+         ["Arithmetic Error","A","B","C"],
+         ["AssertionError","A","B","C"],
+         ["AttributeError","A","B","C"],
+        ]
+
+# Do the quiz
+
+## Ask how many questions the quiz will be and initialize grading scheme
+numQs = int(input(
+         "\nHow many questions would you like to answer? (enter a number):\t"
+        ) ) 
+numCorrect = 0
+numIncorrect = 0
+
+## Actually administer the quiz
+for i in range(numQs):
+    randomNum = random.randint(0, len(fakes)-1)
+    testedBuiltIn = reals[randomNum][0]
+    potentialAnswers = [reals[randomNum][1], fakes[randomNum][1],
+                        fakes[randomNum][1], fakes[randomNum][1] ]
+    random.shuffle(potentialAnswers)
+    print("Question {0}: What is {1}?".format(i, testedBuiltIn))
+    for a in potentialAnswers:
+        print("\t"+a)
+    answer = input("Your Answer?\t").strip()
+    if answer == reals[randomNum][1]:
+        numCorrect += 1
+    else: 
+        numIncorrect += 1
+
+# Output Results
+print("\n ~~~~ Quiz Complete ~~~~ \n")
+print("You got {0} correct out of {1}".format(numCorrect, numQs))
+print("Your final score is {0}%".format(numCorrect/numQs*100))
+print("Thanks for playing!\n")
