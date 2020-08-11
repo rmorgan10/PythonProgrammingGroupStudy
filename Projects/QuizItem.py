9bda77a5b25caf57f11efd7b709531e4e8413c23
abigailbishop <ms.abigailbishop@gmail.com>
2020-08-11 11:49:05 -0500

    Updated setters to account for pandas date formats

--- Projects/QuizItem.py
+++ Projects/QuizItem.py
@@ -14,7 +14,7 @@ class QuizItem:
     __slots__ = ('__word', '__answer', '__date_created', '__date_edited', '__difficulty')
     
     def __init__(self, word, answer, difficulty=0, date_created=None, date_edited=None):
-        self.__date_created = date_created if date_created is not None else datetime.datetime.now()
+        self.date_created = date_created if date_created is not None else datetime.datetime.now()
         self.date_edited = date_edited if date_edited is not None else datetime.datetime.now()
 
         self.word = word
@@ -53,6 +53,18 @@ class QuizItem:
     def date_created(self):
         return self.__date_created
 
+    @date_created.setter
+    def date_created(self, value):
+        if not isinstance(value, datetime.datetime):
+            try: 
+                value = pd.to_datetime(value)
+            except TypeError as err: 
+                raise TypeError(f"No u dummy this is soposed to be a date"
+                            f" loos*r\n{err}")
+            self.__date_created = value
+        else:
+            self.__date_created = value
+
     @property
     def date_edited(self):
         return self.__date_edited
@@ -60,8 +72,12 @@ class QuizItem:
     @date_edited.setter
     def date_edited(self, value):
         if not isinstance(value, datetime.datetime):
-            raise TypeError("No u dummy this is soposed to be a date"
-                            " loos*r")
+            try: 
+                value = pd.to_datetime(value)
+            except TypeError as err: 
+                raise TypeError(f"No u dummy this is soposed to be a date"
+                            f" loos*r\n{err}")
+            self.__date_edited = value
         else:
             self.__date_edited = value
 
